e05f09d8a980a492ad5c135fd96f0b85efd16000
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-10-06 15:59:28 +0100

    feat: add nav admin main

---
+++ src/utils/nav-main-admin.ts
@@ -0,0 +1,121 @@
+import {
+  LayoutDashboard,
+  Users,
+  School,
+  FileSpreadsheet,
+  ClipboardList,
+  Settings2,
+} from "lucide-react";
+import { ROUTES } from "./route";
+
+export const NavMainAdmin = [
+  {
+    title: "Tableau de bord",
+    icon: LayoutDashboard,
+    url: ROUTES.DASHBOARD
+  },
+  {
+    title: "Utilisateurs",
+    url: "#",
+    icon: Users,
+    isActive: true,
+    items: [
+      {
+        title: "Liste des utilisateurs",
+        url: "#",
+      },
+      {
+        title: "Ajouter un utilisateur",
+        url: "#",
+      },
+      {
+        title: "Rôles & permissions",
+        url: "#",
+      },
+      {
+        title: "Historique des connexions",
+        url: "#",
+      },
+    ],
+  },
+  {
+    title: "Classes",
+    url: "#",
+    icon: School,
+    items: [
+      {
+        title: "Liste des classes",
+        url: "#",
+      },
+      {
+        title: "Créer une classe",
+        url: "#",
+      },
+      {
+        title: "Affecter les élèves",
+        url: "#",
+      },
+      {
+        title: "Emploi du temps",
+        url: "#",
+      },
+    ],
+  },
+  {
+    title: "Matières",
+    url: "#",
+    icon: FileSpreadsheet,
+    items: [
+      {
+        title: "Liste des matières",
+        url: "#",
+      },
+      {
+        title: "Ajouter une matière",
+        url: "#",
+      },
+      {
+        title: "Affecter aux professeurs",
+        url: "#",
+      },
+    ],
+  },
+  {
+    title: "Notes",
+    url: "#",
+    icon: ClipboardList,
+    items: [
+      {
+        title: "Toutes les notes",
+        url: "#",
+      },
+      {
+        title: "Ajouter des notes",
+        url: "#",
+      },
+      {
+        title: "Statistiques / Moyennes",
+        url: "#",
+      },
+    ],
+  },
+  {
+    title: "Paramètres",
+    url: "#",
+    icon: Settings2,
+    items: [
+      {
+        title: "Informations de l’école",
+        url: "#",
+      },
+      {
+        title: "Équipe administrative",
+        url: "#",
+      },
+      {
+        title: "Sécurité & accès",
+        url: "#",
+      },
+    ],
+  },
+];
