3778f615c79eed80f95817347b46896e7052fb72
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-10-06 22:32:32 +0100

    feat: add ModeToogle in the SidebarBreadcrumb

--- src/components/dashboard/sidebar-breadcrumb.tsx
+++ src/components/dashboard/sidebar-breadcrumb.tsx
@@ -9,6 +9,7 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "../ui/breadcrumb";
+import ModeToggle from "../mode-toggle";
 
 type breadcrumbProps = {
   label?: string;
@@ -17,7 +18,7 @@ type breadcrumbProps = {
 export default function SidebarBreadcrumb({label}:breadcrumbProps) {
   return (
     <div>
-      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+      <header className="flex justify-between h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
         <div className="flex items-center gap-2 px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator
@@ -37,6 +38,9 @@ export default function SidebarBreadcrumb({label}:breadcrumbProps) {
               </BreadcrumbItem>
             </BreadcrumbList>
           </Breadcrumb>
+        </div>
+        <div className="pe-4">
+          <ModeToggle/>
         </div>
       </header>
     </div>
